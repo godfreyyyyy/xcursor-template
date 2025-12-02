8413129c71a942a0c440846dc6c1396d34341e34
Nyvyme <mixagame228@gmail.com>
2025-12-02 22:41:51 +0500

    Comments in `vars.sh`.

--- vars.sh
+++ vars.sh
@@ -3,16 +3,20 @@
 # This file contains all variables which are needed to compile and install
 # cursor themes. Feel free to change them.
 
+# Theme name
 theme_name="XCursor Template Cursor Theme"
+
+# Theme comment
 theme_comment=""
 
 path_theme_name="xcursor_template_theme"
 src_dir="src"
-builds_dir="build"
-cursors_dir="cursors"
 hotspots_dir="hotspots"
 symlinks_dir="symlinks"
-
 imgs_dir="imgs"
+cursors_dir="cursors"
+builds_dir="build"
 
+# Resolutions for which cursor themes will be generated.
+# At least one resolution must be provided.
 resolutions=(16 24 32 48 64 72 80 96 128)
